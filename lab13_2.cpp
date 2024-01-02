da8457b0775411a5e6053e6c0e10586e40033c93
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2024-01-02 17:04:38 +0700

    add  lab13_2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool table[N][M]){
+    for (int i= 0; i < M + 2; i++) cout << "-";
+    cout << endl;
+    for (int j = 0; j < N; j++){
+        cout << "|";
+        for (int i = 0; i < M; i++){
+            if (table[j][i] != 1){
+                cout << " ";
+            }
+            else{
+                cout << "*";
+            }
+        }
+        cout << "|" << endl;
+    }
+    for (int i= 0; i < M + 2; i++) cout << "-";
+    cout << "\n";
+    }
+
+void updateImage(bool img[N][M], int s, int x, int y){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            if (sqrt(pow(i-x, 2) + pow(j-y, 2)) <= s-1){
+                img[i][j] = 1;
+            }    
+        }
+    }
+}
